0793c1a5f7fbc9c090919c2289e3603253ed968b
Henrique <henriquelr2001@gmail.com>
2025-03-17 19:20:14 -0300

    Iniciando módulo de produtos.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return response()->json(Product::all());
     }
 
     /**
@@ -20,30 +20,34 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $products = Product::create($request->all());
+        return response()->json($products, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Products $products)
+    public function show(Product $products)
     {
-        //
+        return response()->json(Product::findOrFail($products->id));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Products $products)
+    public function update(Request $request, Product $products)
     {
-        //
+        $products = Product::findOrFail($products);
+        $products->update($request->all());
+        return response()->json($products, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Products $products)
+    public function destroy(Product $products)
     {
-        //
+        Product::destroy($products);
+        return response()->json(['message'=> 'Produto deletado']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::get('products', [ProductController::class, 'index']);
+    
+Route::apiResource('products', ProductController::class);
